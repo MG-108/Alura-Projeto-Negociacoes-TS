836e8ca1b9d75234afb5af11d21574d6027b1ce4
Matheus Ghignatti <matheusghignatti@gmail.com>
2022-10-11 15:44:42 -0300

    parte 1 finalizada

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,11 +1,12 @@
 export class Negociacao {
+   
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
 
-    get volume(): number {
+    get volume (): number {
         return this.quantidade * this.valor;
     }
 
